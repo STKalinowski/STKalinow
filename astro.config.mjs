686353baf56ada9ec8549d63475cbb6ffae665df
STKalinowski <kalinowski.stas@gmail.com>
2023-04-04 10:46:40 -0700

    Update Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://STKalinow.github.io',
+    site: 'https://stkalinowski.github.io',
     base: '/STKalinow',
 });
